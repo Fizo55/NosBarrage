af14d6e3cc60d10547c58f8e2ab052793f33d80c
Fizo55 <hugomacees56000@gmail.com>
2023-02-06 10:36:54 +0100

    chore: add arguments to constructor

--- src/NosBarrage.Shared/Args/Login/NoS0575PacketArgs.cs
+++ src/NosBarrage.Shared/Args/Login/NoS0575PacketArgs.cs
@@ -2,6 +2,12 @@
 {
     public class NoS0575PacketArgs
     {
+        public NoS0575PacketArgs(string sessionId, string name)
+        {
+            SessionId = sessionId;
+            Name = name;
+        }
+
         public string SessionId { get; set; }
 
         public string Name { get; set; }
