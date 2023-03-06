01d3ab86bd1372a0ed14f9ab335767a7f80d577b
Fizo55 <hugomacees56000@gmail.com>
2023-03-06 17:08:57 +0100

    chore: fix login cryptography

--- src/NosBarrage.Core/Cryptography/LoginCryptography.cs
+++ src/NosBarrage.Core/Cryptography/LoginCryptography.cs
@@ -4,17 +4,15 @@ public class LoginCryptography
 {
     public static byte[] LoginEncrypt(byte[] packet)
     {
-        byte[] output;
-
         if (packet[^1] != 0xA)
             packet = packet.Concat("\n"u8.ToArray()).ToArray();
 
-        output = new byte[packet.Length];
+        byte[] output = new byte[packet.Length];
         for (int i = 0; i < packet.Length; i++)
         {
             byte b = packet[i];
-            byte v = (byte)(b + 0xF);
-            output[i] = (byte)(v & 0xFF);
+            byte v = (byte)(((b ^ 0xC3) + 0xF) & 0xFF);
+            output[i] = v;
         }
 
         return output;
