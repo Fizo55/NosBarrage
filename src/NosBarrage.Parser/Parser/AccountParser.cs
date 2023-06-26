f52c1c3c00d092835c0e58f5da95de3e66ca4934
Fizo55 <hugomacees56000@gmail.com>
2023-06-26 23:05:01 +0200

    chore: fix multiple insertion if running more time than once

--- src/NosBarrage.Parser/Parser/AccountParser.cs
+++ src/NosBarrage.Parser/Parser/AccountParser.cs
@@ -20,9 +20,11 @@ public class AccountParser : IParser
     public async Task ParseAsync()
     {
         AccountEntity account = new(AccountId: 0, Username: "Fizo", Password: "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff", Authority: Authority.Administrator);
-        await _accountEntity.AddAsync(account);
+        if (!await _accountEntity.AnyAsync(s => s.Username == account.Username))
+            await _accountEntity.AddAsync(account);
         AccountEntity account2 = new(AccountId: 0, Username: "Test", Password: "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff", Authority: Authority.User);
-        await _accountEntity.AddAsync(account2);
+        if (!await _accountEntity.AnyAsync(s => s.Username == account2.Username))
+            await _accountEntity.AddAsync(account2);
         _logger.Debug("Accounts parsed");
     }
 }
